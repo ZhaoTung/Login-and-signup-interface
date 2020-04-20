17ba0da88c871ce92ac428001eca2c1a0efa7ff7
Zhao Feng Tung <52980661+ZhaoTung@users.noreply.github.com>
2020-04-19 22:23:53 -0400

    first try

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.parse.ParseInstallation;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+    }
+}
